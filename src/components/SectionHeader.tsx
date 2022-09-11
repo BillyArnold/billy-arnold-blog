8d4edd55d90aa5164ebc6282752fc6184a1a95d3
Billy Arnold <billyarnoldd@gmail.com>
2022-09-11 19:27:01 +0100

    Setup of initial styles and favicon

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,20 @@
+import { motion } from "framer-motion";
+
+const SectionHeader = (props) => {
+    return (
+      <>
+      <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ duration: 1 }}
+            >
+        <div className="flex justify-center w-full px-5 md:px-20 pt-6 pb-20 font-semibold text-xl">
+          <div>{props.children}</div>
+        </div>
+        </motion.div>
+      </>
+    )
+  }
+
+export default SectionHeader;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: ["poppins", "sans-serif"],
+    },
+    extend: {
+      fontFamily: {
+        sans: ["poppins", "sans-serif"],
+      },
+      colors: {
+        beigeBG: "#f5eed0",
+        mainDark: "#000",
+        emeraldGreen: "#177369",
+      },
+    },
   },
   plugins: [],
 };
